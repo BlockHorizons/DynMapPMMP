08278639be91242ac364f947aae1992db2f7f7df
Sandertv <st.ten.veldhuis@gmail.com>
2017-09-10 09:55:37 +0200

    Initial commit

---
+++ plugin/src/BlockHorizons/DynMapPMMP/DynMapPMMP.php
@@ -0,0 +1,18 @@
+<?php
+
+declare(strict_types = 1);
+
+namespace BlockHorizons\DynMapPMMP;
+
+use pocketmine\plugin\PluginBase;
+
+class DynMapPMMP extends PluginBase {
+
+	public function onEnable(): void {
+
+	}
+
+	public function onDisable(): void {
+
+	}
+}
